16279cb27af93c80cf5eef90ce49f55556dce969
hori-ryota <hori_ryota@cyberagent.co.jp>
2018-02-10 16:01:05 +0900

    update

--- main_test.go
+++ main_test.go
@@ -18,7 +18,7 @@ func BenchmarkWithoutPool(b *testing.B) {
 		name := strconv.Itoa(count)
 		b.Run(name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				_ = make([]Chunk, count)
+				_ = make([]Chunk, 0, count)
 			}
 		})
 	}
@@ -31,7 +31,7 @@ func BenchmarkWithPool(b *testing.B) {
 		b.Run(name, func(b *testing.B) {
 			pool := sync.Pool{
 				New: func() interface{} {
-					return make([]Chunk, count)
+					return make([]Chunk, 0, count)
 				},
 			}
 			b.ResetTimer()
@@ -42,3 +42,97 @@ func BenchmarkWithPool(b *testing.B) {
 		})
 	}
 }
+
+// func BenchmarkWithPoolAndInitStruct(b *testing.B) {
+// 	testCounts := []int{0, 1, 10, 100, 1000, 10000}
+// 	for _, count := range testCounts {
+// 		name := strconv.Itoa(count)
+// 		b.Run(name, func(b *testing.B) {
+// 			pool := sync.Pool{
+// 				New: func() interface{} {
+// 					return make([]Chunk, count)
+// 				},
+// 			}
+// 			b.ResetTimer()
+// 			for i := 0; i < b.N; i++ {
+// 				c := pool.Get().([]Chunk)
+// 				for j := range c {
+// 					c[j] = Chunk{}
+// 				}
+// 				pool.Put(c)
+// 			}
+// 		})
+// 	}
+// }
+
+// func BenchmarkWithPoolAndInitFields(b *testing.B) {
+// 	testCounts := []int{0, 1, 10, 100, 1000, 10000}
+// 	for _, count := range testCounts {
+// 		name := strconv.Itoa(count)
+// 		b.Run(name, func(b *testing.B) {
+// 			pool := sync.Pool{
+// 				New: func() interface{} {
+// 					return make([]Chunk, count)
+// 				},
+// 			}
+// 			b.ResetTimer()
+// 			for i := 0; i < b.N; i++ {
+// 				c := pool.Get().([]Chunk)
+// 				for j := range c {
+// 					c[j].SeqId = 0
+// 					c[j].Duration = 0
+// 					c[j].URI = ""
+// 				}
+// 				pool.Put(c)
+// 			}
+// 		})
+// 	}
+// }
+
+// func (c *Chunk) Init() {
+// 	c.SeqId = 0
+// 	c.Duration = 0
+// 	c.URI = ""
+// }
+
+// func BenchmarkWithPoolAndInitFunc(b *testing.B) {
+// 	testCounts := []int{0, 1, 10, 100, 1000, 10000}
+// 	for _, count := range testCounts {
+// 		name := strconv.Itoa(count)
+// 		b.Run(name, func(b *testing.B) {
+// 			pool := sync.Pool{
+// 				New: func() interface{} {
+// 					return make([]Chunk, count)
+// 				},
+// 			}
+// 			b.ResetTimer()
+// 			for i := 0; i < b.N; i++ {
+// 				c := pool.Get().([]Chunk)
+// 				for j := range c {
+// 					c[j].Init()
+// 				}
+// 				pool.Put(c)
+// 			}
+// 		})
+// 	}
+// }
+
+// func BenchmarkWithPoolAndSetLenZero(b *testing.B) {
+// 	testCounts := []int{0, 1, 10, 100, 1000, 10000}
+// 	for _, count := range testCounts {
+// 		name := strconv.Itoa(count)
+// 		b.Run(name, func(b *testing.B) {
+// 			pool := sync.Pool{
+// 				New: func() interface{} {
+// 					return make([]Chunk, count)
+// 				},
+// 			}
+// 			b.ResetTimer()
+// 			for i := 0; i < b.N; i++ {
+// 				c := pool.Get().([]Chunk)
+// 				c = c[0:0]
+// 				pool.Put(c)
+// 			}
+// 		})
+// 	}
+// }
